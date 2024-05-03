20c320298bae2a13e60f220ea7fc75cf8668b571
jusmrcrd <ricardo.m.gama@hotmail.com>
2024-05-03 10:52:49 -0300

    test

--- ParkPairPrograming/src/Adult.java
+++
@@ -1,8 +0,0 @@
-public class Adult extends Person {
-    Adult(String name,int age){
-        super(name,age);
-        if(age < 18){
-            throw new RuntimeException("não e um adulto");
-        }
-    }
-}
